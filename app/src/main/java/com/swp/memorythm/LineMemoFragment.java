a197b1fa92f96e9c2e5ab131f670c08bed3553ea
jaeseuk <sjs4126@naver.com>
2020-12-21 21:51:19 +0900

    줄 메모 구현완료, 데일리 플랜 구현중 [구현완료] 1. 줄 메모 [구현중] 1. 데일리 플랜 [수정] 1. themes.xml 스타일 추가 [추가(파일 또는 기능)] 1. DailyPlanFragment 2. LineEditText 3. LineMemoFragment 4. template_dailyplan.xml 5. template_linememo.xml 6. 날씨 아이콘 및 셀렉터 drawable

---
+++ app/src/main/java/com/swp/memorythm/LineMemoFragment.java
@@ -0,0 +1,75 @@
+package com.swp.memorythm;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class LineMemoFragment extends Fragment {
+    private TextView textViewDate;
+    private EditText editTextContent;
+
+    public static LineMemoFragment newInstance() {
+        return new LineMemoFragment();
+    }
+
+    // 캘린더 객체 생성
+    Calendar myCalendar = Calendar.getInstance();
+
+    //데이트픽커 다이얼로그
+    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    // 텍스트뷰 날짜 업데이트
+    private void updateLabel() {
+        String DateFormat = "yyyy - MM - dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(DateFormat, Locale.KOREA);
+
+        textViewDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.template_linememo, container, false);
+
+        textViewDate = rootView.findViewById(R.id.write_date);
+        editTextContent = rootView.findViewById(R.id.memo_content);
+
+        // 텍스트뷰 초기 날짜 현재 날짜로 설정
+        Date currentTime = Calendar.getInstance().getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy - MM - dd", Locale.KOREA);
+        textViewDate.setText(simpleDateFormat.format(currentTime));
+
+        textViewDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // 데이트픽커 띄우기
+                new DatePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        // TODO: 2020-11-20 파이어베이스 연동
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/swp/memorythm/LinedEditText.java
@@ -0,0 +1,67 @@
+package com.swp.memorythm;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.viewpager2.widget.ViewPager2;
+
+//EditText 밑줄 그어주는 클래스
+public class LinedEditText extends AppCompatEditText {
+    private Paint mPaint = new Paint();
+    private int inc_count = 0; //count 값 증가해주기 위한 전역 변수
+
+    public LinedEditText(Context context) {
+        super(context);
+        initPaint();
+    }
+
+    public LinedEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPaint();
+    }
+
+    public LinedEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(0x80000000);
+    }
+
+    @Override protected void onDraw(Canvas canvas) {
+        int right = getRight();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        int lineHeight = getLineHeight();
+        int height = getHeight();
+        float textSize = getTextSize();
+        int count = (height-paddingTop-paddingBottom) / lineHeight + inc_count;
+
+        for (int i = 0; i < count; i++) {
+            float baseline = lineHeight * (i+1) + paddingTop - textSize / 2;
+            canvas.drawLine(0, baseline, right-paddingRight, baseline, mPaint);
+        }
+
+        super.onDraw(canvas);
+    }
+
+    //스크롤 변할때마다 밑줄 추가해줌
+    @Override
+    protected void onScrollChanged(int horiz, int vert, int oldHoriz, int oldVert) {
+        super.onScrollChanged(horiz, vert, oldHoriz, oldVert);
+
+        inc_count++;
+        invalidate(); //onDraw 다시 호출하는 함수
+    }
+}
