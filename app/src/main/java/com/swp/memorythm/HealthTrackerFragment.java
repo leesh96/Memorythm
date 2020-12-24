495f91d027e1d391ae5532ff5307fb79275cea5a
YoonGyeong <14dnfnfn@gmail.com>
2020-12-24 23:02:40 +0900

    헬스 트래커 구현중 [구현완료] [구현중] 1. 헬스 트래커 레이아웃 거의 완성 2. 헬스 트래커 운동부위 부분 마무리 남음 [수정] [추가(파일 또는 기능)] 1. PreferenceManager

--- app/src/main/java/com/swp/memorythm/HealthTrackerFragment.java
+++ app/src/main/java/com/swp/memorythm/HealthTrackerFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
-import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,8 +20,9 @@ import java.util.Locale;
 
 public class HealthTrackerFragment extends Fragment {
     private TextView textViewDate;
+    private ImageButton ibtn_cup1,ibtn_cup2,ibtn_cup3,ibtn_cup4,ibtn_cup5,ibtn_cup6,ibtn_cup7,ibtn_cup8;
 
-
+    private int cup1, cup2, cup3, cup4, cup5, cup6, cup7, cup8;
     public static HealthTrackerFragment newInstance() {
         return new HealthTrackerFragment();
     }
@@ -54,22 +55,143 @@ public class HealthTrackerFragment extends Fragment {
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.template_healthtracker, container, false);
 
         textViewDate = viewGroup.findViewById(R.id.write_date);
-
+        ibtn_cup1 = viewGroup.findViewById(R.id.ibtn_cup1);
+        ibtn_cup2 = viewGroup.findViewById(R.id.ibtn_cup2);
+        ibtn_cup3 = viewGroup.findViewById(R.id.ibtn_cup3);
+        ibtn_cup4 = viewGroup.findViewById(R.id.ibtn_cup4);
+        ibtn_cup5 = viewGroup.findViewById(R.id.ibtn_cup5);
+        ibtn_cup6 = viewGroup.findViewById(R.id.ibtn_cup6);
+        ibtn_cup7 = viewGroup.findViewById(R.id.ibtn_cup7);
+        ibtn_cup8 = viewGroup.findViewById(R.id.ibtn_cup8);
 
         // 텍스트뷰 초기 날짜 현재 날짜로 설정
         Date currentTime = Calendar.getInstance().getTime();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy - MM - dd", Locale.KOREA);
         textViewDate.setText(simpleDateFormat.format(currentTime));
 
-        textViewDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { // 데이트픽커 띄우기
-                new DatePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        textViewDate.setOnClickListener(v -> { // 데이트픽커 띄우기
+            new DatePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog, myDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        });
+
+        // 물컵
+        int nweek = myCalendar.get(Calendar.DAY_OF_WEEK); //요일 구하기
+        if(PreferenceManager.getInt(getContext(),"nweek")!=nweek) {
+            PreferenceManager.setInt(getContext(), "cup1", 0);
+            PreferenceManager.setInt(getContext(), "cup2", 0);
+            PreferenceManager.setInt(getContext(), "cup3", 0);
+            PreferenceManager.setInt(getContext(), "cup4", 0);
+            PreferenceManager.setInt(getContext(), "cup5", 0);
+            PreferenceManager.setInt(getContext(), "cup6", 0);
+            PreferenceManager.setInt(getContext(), "cup7", 0);
+            PreferenceManager.setInt(getContext(), "cup8", 0);
+            PreferenceManager.setInt(getContext(),"nweek",nweek);
+        }
+        cup1 = PreferenceManager.getInt(getContext(),"cup1");
+        cup2 = PreferenceManager.getInt(getContext(),"cup2");
+        cup3 = PreferenceManager.getInt(getContext(),"cup3");
+        cup4 = PreferenceManager.getInt(getContext(),"cup4");
+        cup5 = PreferenceManager.getInt(getContext(),"cup5");
+        cup6 = PreferenceManager.getInt(getContext(),"cup6");
+        cup7 = PreferenceManager.getInt(getContext(),"cup7");
+        cup8 = PreferenceManager.getInt(getContext(),"cup8");
+        setCup(ibtn_cup1,cup1); setCup(ibtn_cup2,cup2); setCup(ibtn_cup3,cup3); setCup(ibtn_cup4,cup4);
+        setCup(ibtn_cup5,cup5); setCup(ibtn_cup6,cup6); setCup(ibtn_cup7,cup7); setCup(ibtn_cup8,cup8);
+        ibtn_cup1.setOnClickListener(v -> {
+            if(cup1 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup1", 1);
+                cup1 = 1;
+                ibtn_cup1.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup1", 0);
+                cup1 = 0;
+                ibtn_cup1.setImageResource(R.drawable.icon_blankcup);
             }
         });
+        ibtn_cup2.setOnClickListener(v -> {
+            if(cup2 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup2", 1);
+                cup2 = 1;
+                ibtn_cup2.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup2", 0);
+                cup2 = 0;
+                ibtn_cup2.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+        ibtn_cup3.setOnClickListener(v -> {
+            if(cup3 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup3", 1);
+                cup3 = 1;
+                ibtn_cup3.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup3", 0);
+                cup3 = 0;
+                ibtn_cup3.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+        ibtn_cup4.setOnClickListener(v -> {
+            if(cup4 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup4", 1);
+                cup4 = 1;
+                ibtn_cup4.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup4", 0);
+                cup4 = 0;
+                ibtn_cup4.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+        ibtn_cup5.setOnClickListener(v -> {
+            if(cup5 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup5", 1);
+                cup5 = 1;
+                ibtn_cup5.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup5", 0);
+                cup5 = 0;
+                ibtn_cup5.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+        ibtn_cup6.setOnClickListener(v -> {
+            if(cup6 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup6", 1);
+                cup6 = 1;
+                ibtn_cup6.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup6", 0);
+                cup6 = 0;
+                ibtn_cup6.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+        ibtn_cup7.setOnClickListener(v -> {
+            if(cup7 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup7", 1);
+                cup7 = 1;
+                ibtn_cup7.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup7", 0);
+                cup7 = 0;
+                ibtn_cup7.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+        ibtn_cup8.setOnClickListener(v -> {
+            if(cup8 == 0 ){
+                PreferenceManager.setInt(getContext(), "cup8", 1);
+                cup8 = 1;
+                ibtn_cup8.setImageResource(R.drawable.icon_fillcup);
+            }else{
+                PreferenceManager.setInt(getContext(), "cup8", 0);
+                cup8 = 0;
+                ibtn_cup8.setImageResource(R.drawable.icon_blankcup);
+            }
+        });
+
 
         // TODO: 2020-11-20 파이어베이스 연동
 
         return viewGroup;
+    }
+    public void setCup(ImageButton imageButton, int n){
+        if(n==1) imageButton.setImageResource(R.drawable.icon_fillcup);
+        else imageButton.setImageResource(R.drawable.icon_blankcup);
     }
 }
