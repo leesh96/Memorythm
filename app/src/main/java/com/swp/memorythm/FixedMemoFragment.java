0a63bb019c18f38d5c0aabd0c928129e7edb3a1b
Ham-ZZi <hyunjia2@naver.com>
2020-12-30 16:27:49 +0900

    고정메모 프레그먼트 생성
    [구현완료]
    
    [수정]
    1. MainActivity.java
    2. activity_main.xml
    [추가(파일 또는 기능)]
    1. FixedMemoFragment.java
    2. fragment_fixed_memo.xml

---
+++ app/src/main/java/com/swp/memorythm/FixedMemoFragment.java
@@ -0,0 +1,36 @@
+package com.swp.memorythm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+public class FixedMemoFragment extends Fragment {
+    private ViewPager viewPager;
+    private ViewPagerAdapter pagerAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fixed_memo, container, false); // 레이아웃 연결
+
+        //뷰페이저에 페이저어댑터 지정
+        viewPager = (ViewPager)view.findViewById(R.id.pager);
+        viewPager.setOffscreenPageLimit(3); // 3개로 제한
+        pagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+
+        viewPager.setAdapter(pagerAdapter);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
--- app/src/main/java/com/swp/memorythm/MainActivity.java
+++ app/src/main/java/com/swp/memorythm/MainActivity.java
@@ -38,23 +38,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFrag(0);
+
         // 홈버튼 누르면 메인화면으로 돌아가기
         homeBtn = (ImageButton)findViewById(R.id.homeButton);
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                setFrag(0);
             }
         });
 
         //뷰페이저에 페이저어댑터 지정
-        viewPager = (ViewPager)findViewById(R.id.pager);
+/*      viewPager = (ViewPager)findViewById(R.id.pager);
         viewPager.setOffscreenPageLimit(3);
-        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());*/
 /*
-        테스트
         NonlineMemoFragment f1 = new NonlineMemoFragment();
         pagerAdapter.addItem(f1);
         MonthlyPlanFragment f2 = new MonthlyPlanFragment();
@@ -62,7 +62,9 @@ public class MainActivity extends AppCompatActivity {
         MonthTrackerFragment f3 = new MonthTrackerFragment();
         pagerAdapter.addItem(f3);
         viewPager.setAdapter(pagerAdapter);
-*/
+
+ */
+
         //바닥네비게이션 사용
 
         bottomNavigationView  = findViewById(R.id.bottomNavi);
@@ -72,19 +74,19 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.folderItem:
-                        setFrag(0);
-                        break;
-                    case R.id.templateItem:
                         setFrag(1);
                         break;
-                    case R.id.writeItem:
+                    case R.id.templateItem:
                         setFrag(2);
                         break;
-                    case R.id.trashItem:
+                    case R.id.writeItem:
                         setFrag(3);
                         break;
-                    case R.id.setItem:
+                    case R.id.trashItem:
                         setFrag(4);
+                        break;
+                    case R.id.setItem:
+                        setFrag(5);
                         break;
                 }
                 return true;
@@ -105,22 +107,26 @@ public class MainActivity extends AppCompatActivity {
         ft = fm.beginTransaction();
         switch (n){
             case 0:
-                ft.replace(R.id.mainFrame, folderFragment);
+                ft.replace(R.id.mainFrame, new FixedMemoFragment());
                 ft.commit();
                 break;
             case 1:
-                ft.replace(R.id.mainFrame, templateFragment);
+                ft.replace(R.id.mainFrame, new FolderFragment());
                 ft.commit();
                 break;
             case 2:
-                ft.replace(R.id.mainFrame, writeFragment);
+                ft.replace(R.id.mainFrame, new TemplateFragment());
                 ft.commit();
                 break;
             case 3:
-                ft.replace(R.id.mainFrame, trashFragment);
+                ft.replace(R.id.mainFrame, new WriteFragment());
                 ft.commit();
                 break;
             case 4:
+                ft.replace(R.id.mainFrame, trashFragment);
+                ft.commit();
+                break;
+            case 5:
                 ft.replace(R.id.mainFrame, setFragment);
                 ft.commit();
                 break;
